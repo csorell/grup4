6d28c2530e06d1d9a41683d8de7868ad39ce49db
Joan <joanandreusole@gmail.com>
2019-04-14 23:33:55 +0200

    Entorns_fer_JUnit

--- test/entorns/GroupArraysTest.java
+++ test/entorns/GroupArraysTest.java
@@ -6,54 +6,64 @@ import static org.junit.Assert.*;
 public class GroupArraysTest {
     
 
-    //Entre en els dos bucles
-    @Test
-    public void testGroupArrays1() {
-        System.out.println("Dos bucles");
-        int[] array1 = {1,2,3};
-        int[] array2 = {4,5,6};
-        int[] expResult = {1,2,3,4,5,6};
-        int[] result = GroupArrays.groupArrays(array1, array2);
-        assertArrayEquals(expResult, result);
+	//Entra en els dos bucles
+	@Test
+	public void testGroupArrays1() {
+    	System.out.println("Dos bucles");
+    	int[] array1 = {1,2,3};
+    	int[] array2 = {4,5,6};
+    	int[] expResult = {1,2,3,4,5,6};
+    	int[] result = GroupArrays.groupArrays(array1, array2);
+    	assertArrayEquals(expResult, result);
 
-    }
+	}
     
-    //Entre en el primer bucle
-    @Test
-    public void testGroupArrays2() {
-        System.out.println("Primer Bucle");
-        int[] array1 = {1,2,3};
-        int[] array2 = {};
-        int[] expResult = {1,2,3};
-        int[] result = GroupArrays.groupArrays(array1, array2);
-        assertArrayEquals(expResult, result);
+	//Entra en el primer bucle
+	@Test
+	public void testGroupArrays2() {
+    	System.out.println("Primer Bucle");
+    	int[] array1 = {1,2,3};
+    	int[] array2 = {};
+    	int[] expResult = {1,2,3};
+    	int[] result = GroupArrays.groupArrays(array1, array2);
+    	assertArrayEquals(expResult, result);
 
-    }
+	}
     
-    //Entre en el segon bucle 
-    @Test
-    public void testGroupArrays3() {
-        System.out.println("Segon Bucle");
-        int[] array1 = {};
-        int[] array2 = {4,5,6};
-        int[] expResult = {4,5,6};
-        int[] result = GroupArrays.groupArrays(array1, array2);
-        assertArrayEquals(expResult, result);
+	//Entra en el segon bucle
+	@Test
+	public void testGroupArrays3() {
+    	System.out.println("Segon Bucle");
+    	int[] array1 = {};
+    	int[] array2 = {4,5,6};
+    	int[] expResult = {4,5,6};
+    	int[] result = GroupArrays.groupArrays(array1, array2);
+    	assertArrayEquals(expResult, result);
 
-    }
+	}
     
-    //No entre ha cap bucle
-    @Test
-    public void testGroupArrays4() {
-        System.out.println("Cap Bucle");
-        int[] array1 = {};
-        int[] array2 = {};
-        int[] expResult = {};
-        int[] result = GroupArrays.groupArrays(array1, array2);
-        assertArrayEquals(expResult, result);
+	//No entra a cap bucle
+	@Test
+	public void testGroupArrays4() {
+    	System.out.println("Cap Bucle");
+    	int[] array1 = {};
+    	int[] array2 = {};
+    	int[] expResult = {};
+    	int[] result = GroupArrays.groupArrays(array1, array2);
+    	assertArrayEquals(expResult, result);
 
-    }
+	}
     
-    
+	//Entra al if
+    	@Test
+	public void testGroupArrays5() {
+    	System.out.println("Desordenat");
+    	int[] array1 = {1,2,3};
+    	int[] array2 = {4,6,5};
+    	int[] expResult = {1,2,3,4,5,6};
+    	int[] result = GroupArrays.groupArrays(array1, array2);
+    	assertArrayEquals(expResult, result);
+
+	}
     
 }
